434104ac8a513fe80bb8f67fa2aa2ed0d171d4d1
Rust <rudy_curioso@outlook.com>
2025-11-27 03:28:19 -0600

    configuracion de la navegacion

--- app/src/main/java/com/e243768/organipro_/MainActivity.kt
+++ app/src/main/java/com/e243768/organipro_/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.e243768.organipro_.presentation.navigation.NavManager
 import com.e243768.organipro_.ui.theme.OrganiPro_Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +12,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            OrganiPro_Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            OrganiPro_Theme(darkTheme = true) {
+                NavManager()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OrganiPro_Theme {
-        Greeting("Android")
     }
 }
